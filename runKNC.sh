b7681e7f23ac0c95142984f904d994815eec32bf
Tommy Svensson <datee@users.noreply.github.com>
2021-06-01 12:40:56 +0200

    Fix error in path

--- runKNC.sh
+++ runKNC.sh
@@ -2,7 +2,7 @@
 while :
 do
 if [[ $(pidof KontentumNC | wc -l) -eq 0 ]]; then
-    sudo /home/pi/KontentumNC/bin/KontentumNC #run the kontentum client if not already running - (check folder)
+    sudo /home/pi/KontentumNC/KontentumNC #run the kontentum client if not already running - (check folder)
 fi
 sleep 2
 done
